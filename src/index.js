9874b232538bf0c47c0bd5ce18f8927f6f3b7fcc
Karl Thomas <email": null>
2017-04-28 16:09:08 -0500

    Hello world is printing from the screen! also i managed to figure out where react was throwing the data at the screen from

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// it's rendering the app js file here, in the reacting dom guy which makes sense
 ReactDOM.render(
   <App />,
   document.getElementById('root')
