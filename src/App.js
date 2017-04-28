79a75dba5c819090afd984109950c0eae1ead350
Karl Thomas <email": null>
2017-04-28 16:10:53 -0500

    make a comment that elucidates something for me

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+// this app class gets called in the ReactDOM display
 class App extends Component {
   render() {
     return (
