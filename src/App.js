fbd97d466e98349eb43906a22a3a0de3615bca34
Karl Thomas <email": null>
2017-04-28 16:53:13 -0500

    add some fucntions for a quote custom unquote gretting. so far react is fun, repl has a cool service for running the code on your phone

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Karl',
+  lastName: 'Thomas'
+};
+
+function greeting(user){
+  if (user){
+    // here i am using curly braces to shove an expression in the "html"
+    return <h1> Hello, {formatName(user)}! </h1>;
+  }
+  return <h1> Hello, Stranger. </h1>;
+};
+
 // this app class gets called in the ReactDOM display
 class App extends Component {
   render() {
     return (
-      <h1>Hello, world!</h1>
+      greeting(user)
     );
   }
 }
