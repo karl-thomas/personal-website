12101053584916142e056419f95e6c2a95198f27
karl-thomas <karl.thomas925@gmail.com>
2017-04-30 04:01:14 -0500

    start working on a ticking clock i am just follwing the docs right now for getting into react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -28,20 +29,31 @@ const user = {
   lastName: 'Thomas'
 };
 
-function greeting(user){
-  if (user){
-    // here i am using curly braces to shove an expression in the "html"
-    // you can use string literals when declaring attribute names within react.
-    return <h1> Hello, {formatName(user)}! </h1>;
-  }
-  return <h1> Hello, Stranger. </h1>;
+function tick (user){
+  const greeting = (
+    <div>
+      if (user) {
+        // here i am using curly braces to shove an expression in the "html"
+        // you can use string literals when declaring attribute names within react.
+        <h1> Hello, {formatName(user)}!</h1>
+      } 
+      else {
+        <h1> Hello, Stranger. </h1>
+      }
+      <h2> It is {new Date().toLocaleTimeString()}.</h2>
+    </div> 
+    );
+  ReactDOM.render(
+    greeting,
+    document.getElementById('root')
+  ); 
 };
 
 // this app class gets called in the ReactDOM display
 class App extends Component {
   render() {
     return (
-      greeting(user)
+      tick(user)
     );
   }
 }
