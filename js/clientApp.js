f62cabb039f7515af542f7decefadbb7f1c79399
karl-thomas <karl.thomas925@gmail.com>
2017-05-01 23:44:48 -0500

    nested a component within another, just for the fun of it

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,11 +1,24 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var MyTitle = React.createClass({
+	render: function () {
+		return (
+			div(null,
+				h1(null, 'checkout this component')
+			)
+		)
+	}
+})
+
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
       div(null,
-        h1(null, 'This is my first component!')
+        React.createElement(MyTitle),
+        React.createElement(MyTitle),
+        React.createElement(MyTitle),
+        React.createElement(MyTitle)
       )
     )
   }
