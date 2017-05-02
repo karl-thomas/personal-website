1f607fa779ca1d0a238cd16ae020eb08d035515e
karl-thomas <karl.thomas925@gmail.com>
2017-05-02 00:12:20 -0500

    lint code, update build commands did both using standard, sorry if you don't like semicolons

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,28 +1,28 @@
-/* global React ReactDOM*/
+/* global React ReactDOM */
 
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-	render: function () {
-		return (
+  render: function () {
+    return (
 			div(null,
 				h1({ style: { color: this.props.color, fontWeight: 'bold'}}, this.props.title)
 			)
-		)
-	}
+    )
+  }
 })
 
-var MyTitleFactory = React.createFactory(MyTitle) 
+var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
       div(null,
-        MyTitleFactory({ title: 'props are the best', color:'peru'}),
-        MyTitleFactory({ title: 'semicolons are not', color:'mediumaquamarine'}),
-        MyTitleFactory({ title: 'same # of thingies', color:'rebeccapurple'}),
-        MyTitleFactory({ title: 'woopy woop mc doop', color:'darkvioletred'})
+        MyTitleFactory({title: 'props are the best', color: 'peru'}),
+        MyTitleFactory({title: 'semicolons are not', color: 'mediumaquamarine'}),
+        MyTitleFactory({title: 'same # of thingies', color: 'rebeccapurple'}),
+        MyTitleFactory({title: 'woopy woop mc doop', color: 'darkvioletred'})
       )
     )
   }
