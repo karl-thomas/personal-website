f4c3efeb9cd12cf258747d1f9aa8ede180c97e41
karl-thomas <karl.thomas925@gmail.com>
2017-05-02 16:30:43 -0500

    make it so that my code is linted when webpack is run add more rules to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
   },
   module: {
     rules: [
+    	{
+    		enforce: 'pre',
+    		test: /\.js$/,
+    		loader: 'eslint-loader',
+    		exclude: /node_modules/
+    	},
       {
         include: path.resolve(__dirname, 'js'),
         test: /\.js$/,
