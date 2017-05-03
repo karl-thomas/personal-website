da6799b83a9ccd73984d237d019ace11ec6a8e27
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 17:12:39 -0500

    add unit test to make sure that all my show cards do show up

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -2,9 +2,16 @@ import React from 'react'
 import Search from './Search'
 import { shallow } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
 
 test('Search snapshot test', () => {
 	const component = shallow(<Search />)
 	const tree = shallowToJson(component)
 	expect(tree).toMatchSnapshot()
+})
+
+test ('Search should render a showcard for each show before search', () => {
+	const component = shallow(<Search />)
+	expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
