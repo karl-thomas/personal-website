91572b1592243b540c98e916f87b47b55930fb67
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 01:54:59 -0500

    fixed: add a listener that updates the state on change of the form

--- js/Search.js
+++ js/Search.js
@@ -8,12 +8,15 @@ const Search = React.createClass({
 			searchTerm: 'this is the default string'
 		}
 	},
+	handleSearchTermChange (event) {
+		this.setState({searchTerm: event.target.value})
+	},
 	render () {
 		return (
 			<div className='search'>
 				<header>
-					<h1>{this.state.searchTerm}</h1>
-					<input value={this.state.searchTerm} type='text' placeholder='search' />
+					<h1>searchy guy</h1>
+					<input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='search' />
 				</header>
 				<div>
 					{preload.shows.map((show) => {
