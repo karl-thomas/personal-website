8a5b7837ac882724939ba8146f162b2b7d9f2006
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 01:08:45 -0500

    include my showcard and implement the component

--- js/Search.js
+++ js/Search.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
@@ -7,7 +8,7 @@ const Search = React.createClass({
 			<div className='search'>
 				{preload.shows.map((show) => {
 					return (
-
+						<ShowCard show={show} />
 					)
 				})}
 			</div>
