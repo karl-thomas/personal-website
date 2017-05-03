98e1c0673aae4b519e1fc381f34011040c7cb068
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 01:02:36 -0500

    bust out the showcard into its own file and component** need to destructure, look at that next commit, it's rad

--- js/Search.js
+++ js/Search.js
@@ -7,14 +7,7 @@ const Search = React.createClass({
 			<div className='search'>
 				{preload.shows.map((show) => {
 					return (
-						<div className='show-card'>
-							<img src={`/public/img/posters/${show.poster}`} />
-							<div>
-								<h3>{show.title}</h3>
-								<h4>{show.year}</h4>
-								<p>{show.description}</p>
-							</div>
-						</div>
+
 					)
 				})}
 			</div>
