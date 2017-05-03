10ad4bbf85d3c459a3d290dc8b3558416d2d876c
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 17:21:46 -0500

    rtg test for the search field working correctly

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -15,3 +15,11 @@ test ('Search should render a showcard for each show before search', () => {
 	const component = shallow(<Search />)
 	expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
+
+test ('search should render correct amount of shows based on search', () => {
+	const searchWord = 'house'
+	const component = shallow(<Search />)
+	component.find('input').simulate('change', {target: {value: searchWord}})
+	const showCount = preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
+	expect(component.find(ShowCard).length).toEqual(showCount)
+})
