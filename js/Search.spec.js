67583a9063d6184e7ffbc827fa5485387ecd3704
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 16:26:12 -0500

    configured babelrc, now tests are passing

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,1 +1,9 @@
-Search.spec.js
+import React from 'react'
+import Search from './Search'
+import renderer from 'react-test-renderer'
+
+test('Search snapshot test', () => {
+	const component = renderer.create(<Search />)
+	const tree = component.toJSON()
+	expect(tree).toMatchSnapshot()
+})
