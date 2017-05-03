be9ce9f17317997be77a2a09230c97b15739a561
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 01:31:51 -0500

    refactor with a spread ... operator this effect my props for showcard

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -3,15 +3,13 @@ const { string } = React.PropTypes
 
 const ShowCard = React.createClass ({
 	propTypes:{
-		show: shape({
 			poster: string,
 			title:string,
 			year: string,
 			description: string
-		})
-	}
+	},
 	render(){
-		const {poster, title, year, description} = this.props.show
+		const {poster, title, year, description} = this.props
 		return (
 			<div className='show-card'>
 				<img src={`/public/img/posters/${poster}`} />				
