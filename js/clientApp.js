47596cacedba2064a68ab2dea4a0dfdf17b39fe2
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 00:22:55 -0500

    switch out hashrouter with browserrouter so that my routes are more precise and beautiful

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { HashRouter, Match } from 'react-router'
+import { BrowserRouter, Match } from 'react-router'
 import Landing from './Landing'
 import Search from "./Search"
 import '../public/normalize.css'
