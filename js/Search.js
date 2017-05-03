f3cee6af2decf9cc55235af8b2d464f521cf412f
karl-thomas <karl.thomas925@gmail.com>
2017-05-03 01:43:19 -0500

    broken input when adding state due to no implict two way data binding

--- js/Search.js
+++ js/Search.js
@@ -3,14 +3,25 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+	getInitialState () {
+		return {
+			searchTerm: 'this is the default string'
+		}
+	},
 	render () {
 		return (
 			<div className='search'>
-				{preload.shows.map((show) => {
-					return (
-						<ShowCard key={show.imdbID} {...show} />
-					)
-				})}
+				<header>
+					<h1>{this.state.searchTerm}</h1>
+					<input value={this.state.searchTerm} type='text' placeholder='search' />
+				</header>
+				<div>
+					{preload.shows.map((show) => {
+						return (
+							<ShowCard key={show.imdbID} {...show} />
+						)
+					})}
+				</div>
 			</div>
 		)
 	}
