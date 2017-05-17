e3f58662fbd9c8c992f91e5c5f0b18e838ec5ce0
karl-thomas <karl.thomas925@gmail.com>
2017-05-16 22:44:56 -0500

    adds conditional logic to handle the search option in the header comp

--- js/Header.js
+++ js/Header.js
@@ -3,6 +3,18 @@ import { Link } from 'react-router'
 
 class Header extends React.Component {
 	render () {
+		let utilSpace
+		if (this.props.showSearch){
+			utilSpace = <input onChange={this.props.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='search' />
+		} else {
+			utilSpace = (
+				<h2>
+					<Link to='/search'>
+						Back		
+					</Link>
+				</h2>
+			)
+		}
 		return (
 			<header>
 				<h1>
@@ -10,8 +22,18 @@ class Header extends React.Component {
 						viddy guy
 					</Link>
 				</h1>
+				{utilSpace}
 			</header>
 		)
 	}
 }
+
+const { func, bool, string } = React.PropTypes
+
+Header.propTypes = {
+	handleSearchTermChange: func,
+	showSearch: bool,
+	searchTerm: string
+}
+
 export default Header
