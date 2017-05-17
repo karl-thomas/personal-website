76cf4c7881e60bf313b1edc83a813f46be6346d4
karl-thomas <karl.thomas925@gmail.com>
2017-05-16 22:25:16 -0500

    pulls out the header into it's own component to share across files

---
+++ js/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+class Header extends React.Component {
+	render () {
+		return (
+			<header>
+				<h1>
+					<Link to='/'>
+						viddy guy
+					</Link>
+				</h1>
+			</header>
+		)
+	}
+}
