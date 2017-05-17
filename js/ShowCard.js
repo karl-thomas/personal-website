6f925c31e4631cf6c868ca24b17265104d234d64
karl-thomas <karl.thomas925@gmail.com>
2017-05-16 22:53:53 -0500

    makes it so the show cards are clickable and take you to the correct details page

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import  { Link } from 'react-router'
 const { string } = React.PropTypes
 
 const ShowCard = React.createClass({
@@ -11,14 +12,16 @@ const ShowCard = React.createClass({
   render () {
     const {poster, title, year, description} = this.props
     return (
-			<div className='show-card'>
-				<img src={`/public/img/posters/${poster}`} />
-				<div>
-					<h3>{title}</h3>
-					<h4>{year}</h4>
-					<p>{description}</p>
-				</div>
-			</div>
+      <Link to={`/details/${this.props.imdbID}`}>
+  			<div className='show-card'>
+  				<img src={`/public/img/posters/${poster}`} />
+  				<div>
+  					<h3>{title}</h3>
+  					<h4>{year}</h4>
+  					<p>{description}</p>
+  				</div>
+  			</div>
+      </Link>
 		)
   }
 })
