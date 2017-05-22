b9762e7a4d2814a12d9247229417d28b224d13e5
karl-thomas <karl.thomas925@gmail.com>
2017-05-22 11:05:37 -0500

    succesfully integrate redux in the landing page

--- js/Landing.js
+++ js/Landing.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-
+const { string } =  React.PropTypes
 
 const Landing = React.createClass({
-  render () {
-    return (
-			<div className='landing'>
-				<h1>svideo</h1>
-				<input type='text' placeholder='Search' />
-				<Link to='search'>or Browse All</Link>
-			</div>
-		)
-  }
+	propTypes: {
+		searchTerm: string
+	},
+  	render () {
+	    return (
+				<div className='landing'>
+					<h1>svideo</h1>
+					<input value={this.props.searchTerm} type='text' placeholder='Search' />
+					<Link to='search'>or Browse All</Link>
+				</div>
+			)
+ 	}
 })
 
-export default Landing
+const mapStateToProps = (state) => {
+	return {
+		searchTerm: state.searchTerm
+	}
+}
+
+export default connect(mapStateToProps)(Landing)
 
