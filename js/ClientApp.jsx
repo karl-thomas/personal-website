cef7b754a5ce941cffec22abf474eadc807ad262
karl-thomas <karl.thomas925@gmail.com>
2017-06-29 22:57:42 -0500

    extracts gross script tag garbage to new folder and file

---
+++ js/ClientApp.jsx
@@ -0,0 +1,23 @@
+const ce = React.createElement
+
+const MyTitle = function(props) {
+  return (
+    ce('div', null,
+      ce('h1', {style: {color: props.color}}, props.title)
+      )
+    )
+}
+
+const MyFirstComponent = function (){
+  return ce('div', {id: 'my-first-component'},
+    ce(MyTitle, { title: "game of thrans", color: 'YellowGreen'}),
+    ce(MyTitle, { title: "strangthangs", color: 'GreenYellow'}),
+    ce(MyTitle, { title: "rick marty", color: 'LimeGreen'}),
+    ce(MyTitle, { title: "silly vally", color: 'peru'})
+  )
+}
+
+ReactDOM.render(
+  React.createElement(MyFirstComponent),
+  document.getElementById('app')
+)
