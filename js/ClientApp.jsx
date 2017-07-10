8c1feca564adf9892e8ef66b3e24eb4709324486
karl-thomas <karl.thomas925@gmail.com>
2017-07-09 23:26:21 -0500

    abstract the landing class into its own file to be used as a component

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () =>
-  <div className="app">
-    <div className="landing">
-      <h1>viddy-guy</h1>
-      <input type="text" placeholder="Search" />
-      <a>or browse all</a>
-    </div>
-  </div>;
+const App = () => <div className="app" />;
 
 render(<App />, document.getElementById('app'));
---
+++ js/Landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing = () =>
+  <div className="landing">
+    <h1>viddy-guy</h1>
+    <input type="text" placeholder="Search" />
+    <a>or browse all</a>
+  </div>;
+
+export default Landing;
