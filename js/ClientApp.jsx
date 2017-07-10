ea3387fa429eae47233b92cf579fd17ac67a1229
karl-thomas <karl.thomas925@gmail.com>
2017-07-09 23:33:33 -0500

    starts implement react-router as a service to deterministically load my components

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
 
-const App = () => <div className="app" />;
+const App = () =>
+  <HashRouter>
+    <div className="app">
+      <Route exact path="/" component={Landing} />
+    </div>
+  </HashRouter>;
 
 render(<App />, document.getElementById('app'));
