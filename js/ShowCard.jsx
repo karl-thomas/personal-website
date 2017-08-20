34fa72d7cf42162c294d140d8d4263ea7778fd40
karl-thomas <karl.thomas925@gmail.com>
2017-08-19 20:42:16 -0500

    adds flowtypes to showcard component, defining flowtypes is far more robust type checking than proptypes

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,5 +1,6 @@
+// @flow
+
 import React from 'react';
-import { string } from 'prop-types';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -17,7 +18,8 @@ const Image = styled.img`
   margin-right: 10px;
 `;
 
-const ShowCard = props =>
+// defining the prop types with flow is far more robust type checking then .PropTypes
+const ShowCard = (props: { poster: string, title: string, year: string, description: string }) =>
   <Wrapper>
     <Image src={`/public/img/posters/${props.poster}`} alt={`${props.title} Show Poster`} />
     <div>
@@ -32,12 +34,5 @@ const ShowCard = props =>
       </p>
     </div>
   </Wrapper>;
-
-ShowCard.propTypes = {
-  poster: string.isRequired,
-  title: string.isRequired,
-  year: string.isRequired,
-  description: string.isRequired
-};
 
 export default ShowCard;
