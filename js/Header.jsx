a8ec9f44091af460c4fe255e49bb9e928e829e01
karl-thomas <karl.thomas925@gmail.com>
2017-08-19 22:35:01 -0500

    writes in a little conditional link for the header component, if we are on the details page, we can go back to search results

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,11 +3,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () =>
-  <header>
-    <h1>
-      <Link to="/"> viddy-guy </Link>
-    </h1>
-  </header>;
+// the question mark means its a maybe type
+const Header = (props: { showSearch?: boolean }) => {
+  let utilSpace;
+  if (props.showSearch) {
+    utilSpace = <h1>lol</h1>;
+  } else {
+    utilSpace = (
+      <h2>
+        <Link to="/search"> back</Link>
+      </h2>
+    );
+  }
+
+  return (
+    <header>
+      <h1>
+        <Link to="/"> viddy-guy </Link>
+      </h1>
+
+      {utilSpace}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  showSearch: false
+};
 
 export default Header;
