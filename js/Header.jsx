7a6e8b8ec535e5815461da2298cd21ecb14bb50e
karl-thomas <karl.thomas925@gmail.com>
2017-08-19 22:28:10 -0500

    the header h1 now takes you back to the home page, also places the header component into the details page

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,10 +1,13 @@
 // @flow
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () =>
   <header>
-    <h1>viddy-guy</h1>
+    <h1>
+      <Link to="/"> viddy-guy </Link>
+    </h1>
   </header>;
 
 export default Header;
