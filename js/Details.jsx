68dc1ebeed90ed9c0d988cf7873fd670530e362a
karl-thomas <karl.thomas925@gmail.com>
2017-08-20 02:03:25 -0500

    adds in the loading spinner component to the details page, this works with the component did mount

--- js/Details.jsx
+++ js/Details.jsx
@@ -22,25 +22,41 @@ class Details extends Component {
 
   render() {
     const { title, description, year, poster, trailer } = this.props.show;
+    let ratingComponent;
     // prettier-ignore
+    if (this.state.apiData.rating) {
+      ratingComponent = <h3>{this.state.apiData.rating}</h3>
+    } else {
+      ratingComponent = <Spinner />
+    }
     return (
-    <div className="details">
-      <Header />
-      <section>
-        <h1>{title}</h1>
-        <h2>({year})</h2>
-        <p>{description}</p>
-        <img src={`/public/img/posters/${poster}`} alt={` Poster for ${title} `} />
-      </section>
-      <div>
-        <iframe
-          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
-          frameBorder="0"
-          allowFullScreen
-          title={`Trailer for ${title}`}
-        />
+      <div className="details">
+        <Header />
+        <section>
+          <h1>
+            {title}
+          </h1>
+          <h2>
+            ({year})
+          </h2>
+          {ratingComponent}
+          <p>
+            {description}
+          </p>
+          <img
+            src={`/public/img/posters/${poster}`}
+            alt={` Poster for ${title} `}
+          />
+        </section>
+        <div>
+          <iframe
+            src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder="0"
+            allowFullScreen
+            title={`Trailer for ${title}`}
+          />
+        </div>
       </div>
-    </div>
     );
   }
 }
