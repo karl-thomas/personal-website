14e8feb2cb26e8ccdc120bf0178eb9bd9c62d82a
karl-thomas <karl.thomas925@gmail.com>
2017-08-20 01:22:56 -0500

    Details componentDidMount now makes an axios request to the baked in api, the state handling system in react is so amazing

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,9 +1,21 @@
 // @flow
 
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
+import Spinner from './Spinner';
 
 class Details extends Component {
+  state = {
+    apiData: { rating: '' }
+  };
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3000/${this.props.show.imdbID}`)
+      .then((response: { data: { rating: string } }) => {
+        this.setState({ apiData: response.data });
+      });
+  }
   props: {
     show: Show
   };
