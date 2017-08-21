76a59d154da7c89f68e941c22c539d29fe3abd04
karl-thomas <karl.thomas925@gmail.com>
2017-08-20 21:54:52 -0500

    adds in redux state to the landing component, now it needs to be updated, input is broke

--- js/App.jsx
+++ js/App.jsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
+import { Provider } from 'react-redux'; // redux setup
+import store from './store'; // redux setup
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
@@ -17,17 +19,23 @@ const matchedDetailsPage = (props: { match: Match }) => {
 
 const FourOhFour = () => <h1>404</h1>;
 
+// provider gives app access to redux store
 const App = () =>
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
-        <Route path="/details/:id" component={matchedDetailsPage} />
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route
+            path="/search"
+            component={props => <Search shows={preload.shows} {...props} />}
+          />
+          <Route path="/details/:id" component={matchedDetailsPage} />
 
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>;
 
 export default App;
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,13 +1,18 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Landing = () =>
   <div className="landing">
     <h1>viddy-guy</h1>
-    <input type="text" placeholder="Search" />
+    <input value={props.searchTerm} type="text" placeholder="Search" />
     <Link to="/search"> or browse all </Link>
   </div>;
 
-export default Landing;
+// takes state from redux and injects it into landing
+const mapStateToProps = state => ({ searchTerm: state.searchTerm });
+
+// a decorator, connect(mapStateToProps) returns  FUNCTION which is then invoked with Landing.
+export default connect(mapStateToProps)(Landing);
