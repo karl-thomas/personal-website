0fcef46e61aa4a9257be867ee546c3fec23c2740
karl-thomas <karl.thomas925@gmail.com>
2017-08-20 21:59:10 -0500

    adds flowtypes to props and mapDispatch method to update redux state/ props

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -3,8 +3,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setSearchTerm } from './actionCreators';
 
-const Landing = () =>
+const Landing = (props: { searchTerm: string }) =>
   <div className="landing">
     <h1>viddy-guy</h1>
     <input value={props.searchTerm} type="text" placeholder="Search" />
@@ -14,5 +15,10 @@ const Landing = () =>
 // takes state from redux and injects it into landing
 const mapStateToProps = state => ({ searchTerm: state.searchTerm });
 
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTermChange(event) {
+    dispatch(setSearchTerm(event.target.value));
+  }
+});
 // a decorator, connect(mapStateToProps) returns  FUNCTION which is then invoked with Landing.
-export default connect(mapStateToProps)(Landing);
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
