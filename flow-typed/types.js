78b3db5322853947d76bf1aeaad74dad3f924839
karl-thomas <karl.thomas925@gmail.com>
2017-08-24 20:39:39 -0500

    refactors reducers.js to use combine reducers, now the responsibility of of setting search term is the search term reducer

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -17,9 +17,10 @@ declare var module: {
 
 declare type ActionType = 'SET_SEARCH_TERM';
 
-declare type ActionT<A: ActionType, P> {
-  type: A, 
+// bars needed for generic types
+declare type ActionT<A: ActionType, P> = {|
+  type: A,
   payload: P
-}
+|};
 
 export type Action = ActionT<'SET_SEARCH_TERM', string>;
--- js/reducers.js
+++ js/reducers.js
@@ -1,20 +1,29 @@
 // @flow
-
+import { combineReducers } from 'redux';
 import { SET_SEARCH_TERM } from './actions';
 
-const DEFAULT_STATE = {
-  searchTerm: ''
-};
+// // i no longer need this because the searchterm reducer takes care of its own default state
+// const DEFAULT_STATE = {
+//   searchTerm: ''
+// };
 
-const setSearchTerm = (state, action) => Object.assign({}, state, { searchTerm: action.payload });
-
-const rootReducer = (state = DEFAULT_STATE, action) => {
-  switch (action.type) {
-    case SET_SEARCH_TERM:
-      return setSearchTerm(state, action);
-    default:
-      return state;
+const searchTerm = (state, action: Action) => {
+  if (action.type === SET_SEARCH_TERM) {
+    return action.payload;
   }
+  return state;
 };
+
+const rootReducer = combineReducers({ searchTerm }); // the same as {searchTerm: searchTerm}
+
+// no longer need this because the combine reducers takes care of it for me.
+// const rootReducer = (state = DEFAULT_STATE, action) => {
+//   switch (action.type) {
+//     case SET_SEARCH_TERM:
+//       return setSearchTerm(state, action);
+//     default:
+//       return state;
+//   }
+// };
 
 export default rootReducer;
