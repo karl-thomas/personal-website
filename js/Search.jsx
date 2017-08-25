6fd443b6f26cab0f363dedcb500484f17d8792b4
karl-thomas <karl.thomas925@gmail.com>
2017-08-24 19:47:41 -0500

    coverts search to a functional component to use redux to manage state

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,8 +5,7 @@ import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import Header from './Header';
 
-class Search extends Component {
-  /* -*- this is replaced with class properties.
+/* -*- this is replaced with class properties.
    constructor(props) {
      super(props);
      this.state = {
@@ -15,41 +14,33 @@ class Search extends Component {
    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
    } */
 
-  state = {
-    searchTerm: ''
-  };
+// state = {
+//   searchTerm: ''
+// };
 
-  props: {
-    // shows? that means maybe i'll have that prop
-    shows: Array<Show>
-  };
-  // this is an auto bind, it replaces function = function.bind(this) in the constructor
-  // it does not create a new context
-  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-    this.setState({ searchTerm: event.target.value });
-  };
-
-  render() {
-    return (
-      <div className="search">
-        <Header
-          showSearch
-          searchTerm={this.state.searchTerm}
-          handleSearchTermChange={this.handleSearchTermChange}
-        />
-        <div>
-          {this.props.shows
-            .filter(
-              show =>
-                `${show.title} ${show.description}`
-                  .toUpperCase()
-                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
-            )
-            .map(show => <ShowCard key={show.imdbID} {...show} />)}
-        </div>
-      </div>
-    );
-  }
-}
+// props: {
+// shows? that means maybe i'll have that prop
+//   shows: Array<Show>
+// };
+// this is an auto bind, it replaces function = function.bind(this) in the constructor
+// it does not create a new context
+// handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
+//   this.setState({ searchTerm: event.target.value });
+// };
+const Search = (props: {
+  searchTerm: string, // eslint-disable-line react/no-unused-prop-types
+  shows: Array<Show>
+}) =>
+  <div className="search">
+    <Header showSearch />
+    <div>
+      {props.shows
+        .filter(
+          show =>
+            `${show.title} ${show.description}`.toUpperCase().indexOf(props.searchTerm.toUpperCase()) >= 0
+        )
+        .map(show => <ShowCard key={show.imdbID} {...show} />)}
+    </div>
+  </div>;
 
 export default Search;
