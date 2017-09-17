9f6a7ab797fa67421ba95cb66bf90e34b26e1822
karl-thomas <karl.thomas925@gmail.com>
2017-09-16 19:07:25 -0500

    first draft of a media breakpoint thing creator made, a basic reduc on Object to map breakpoints to sizes

--- js/PostContainer.jsx
+++ js/PostContainer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import media from './breakpoints';
 
 const Wrapper = styled.div`
   position: fixed;
