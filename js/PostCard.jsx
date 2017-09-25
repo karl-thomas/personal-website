cb3cf11937f2700bbc46b1ba61c929256f6a39b7
karl-thomas <karl.thomas925@gmail.com>
2017-09-25 00:56:23 -0500

    adds timestamps to the post card

--- js/PostCard.jsx
+++ js/PostCard.jsx
@@ -2,19 +2,47 @@
 
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { colors } from './utilities';
 // to take me to the details page in the future
 // import { Link } from 'react-router-dom';
 
 const Card = styled.div`
-  background-color: rgba(156, 196, 205, 1);
+  background-color: ${colors.lightBlue};
   margin: 0em 0em 1em 0em;
   position: relative;
+  padding: 1em;
   height: 150px;
   width: 100%;
 `;
 
-const Title = styled.h1`color: white;`;
-const CountBox = styled.div`color: white;`;
+const Title = styled.h1`
+  display: inline-block;
+  color: white;
+`;
+const CountBox = styled.div`
+  color: white;
+  float: right;
+`;
+
+const TimeBox = styled.div`
+  background-color: ${colors.blueShadow};
+  position: absolute;
+  padding: 0em 1em;
+  border-top: thin solid black;
+  right: 0em;
+  height: 30px;
+  width: 100%;
+  bottom: 0px;
+`;
+
+const TimeStamp = styled.p`
+  margin: 4px 0px 0px 0px;
+  padding: 0px;
+  float: right;
+  top: 0px;
+  font-style: italic;
+`;
+
 class PostCard extends Component {
   componentDidMount() {
     console.log(this.props); // eslint-disable-line no-console
@@ -24,7 +52,8 @@ class PostCard extends Component {
     // github_record: Object, // not using this yet
     // spotify_record: Object,
     // twitter_record: Object,
-    total_interactions: number
+    total_interactions: number,
+    created_at: string
   };
 
   render() {
@@ -34,6 +63,9 @@ class PostCard extends Component {
         <CountBox>
           <h1>{this.props.total_interactions}</h1>
         </CountBox>
+        <TimeBox>
+          <TimeStamp>{this.props.created_at}</TimeStamp>
+        </TimeBox>
       </Card>
     );
   }
