9c2821badc2196f8bdf4cdc61238d8dee7292847
karl-thomas <karl.thomas925@gmail.com>
2017-09-25 00:00:22 -0500

    after messing with the api, adds counts to the post card

--- js/PostCard.jsx
+++ js/PostCard.jsx
@@ -9,23 +9,33 @@ const Card = styled.div`
   background-color: rgba(156, 196, 205, 1);
   margin: 0em 0em 1em 0em;
   position: relative;
-  overflow-y: scroll;
-  height: 200px;
+  height: 150px;
   width: 100%;
 `;
+
+const Title = styled.h1`color: white;`;
+const CountBox = styled.div`color: white;`;
 class PostCard extends Component {
   componentDidMount() {
     console.log(this.props); // eslint-disable-line no-console
   }
   props: {
-    id: String // , not using these yet but will be.
-    // github_record: Object,
+    id: String,
+    // github_record: Object, // not using this yet
     // spotify_record: Object,
-    // twitter_recod: Object
+    // twitter_record: Object,
+    total_interactions: number
   };
 
   render() {
-    return <Card>{this.props.id}</Card>;
+    return (
+      <Card>
+        <Title>{this.props.id}</Title>
+        <CountBox>
+          <h1>{this.props.total_interactions}</h1>
+        </CountBox>
+      </Card>
+    );
   }
 }
 export default PostCard;
