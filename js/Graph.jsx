a20ec43dc1cee0579ac9e4d24ed683305b70bdc6
karl-thomas <karl.thomas925@gmail.com>
2017-10-23 22:11:02 -0500

    bring over raw d3.js from a jsfiddle i was working on

--- js/Graph.jsx
+++ js/Graph.jsx
@@ -3,12 +3,72 @@ import React, { Component } from 'react';
 class Graph extends Component {
   props = {};
 
+  componentDidMount(){
+    draw(this.data)
+  }
+
   convertToSimpleData = start => {
     Object.keys(start).map(date => ({
       date,
       data: Object.values(start[date]).reduce((a, b) => a + b, 0)
     }));
   };
+
+  var data = convertToSimpleData(startingData)
+  var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var parseTime = d3.timeParse("%Y-%m-%d");
+
+var x = d3.scaleTime()
+    .rangeRound([0, width]);
+
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+var line = d3.line()
+    .curve(d3.curveCatmullRomOpen)
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.data); });
+
+var draw = (data) => { 
+  data.forEach((d) =>{ 
+    d.date = parseTime(d.date)
+    d.data = +d.data
+  })
+  console.log(data)
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.data; }));
+
+  g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+    .select(".domain")
+      ;
+
+  g.append("g")
+      .call(d3.axisLeft(y))
+    .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Price ($)");
+
+  g.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+};
+  
   render() {
     return <div> Oh Hello!!!</div>;
   }
