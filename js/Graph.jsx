c6c0fc70a3cec326fd815b09a3b30550c170faa1
karl-thomas <karl.thomas925@gmail.com>
2017-11-05 14:57:42 -0600

    multi lin graph now works, refactoring

--- js/Graph.jsx
+++ js/Graph.jsx
@@ -22,94 +22,8 @@ class Graph extends Component {
   componentDidMount() {
     const githubData = this.convertToSimpleData(this.props, 'Github');
     const spotifyData = this.convertToSimpleData(this.props, 'Spotify');
-    const datafy = (...data) => {
-      const dataObj = {};
-      dataObj.columns = ['Github', 'Spotify'];
-      console.log(data);
-    };
-    datafy(githubData, spotifyData);
-
-    const parseTime = d3.timeParse('%Y%m%d');
-    const type = (d, _, columns) => {
-      d.date = parseTime(d.date);
-      for (let i = 1, n = columns.length, c; i < n; i += 1) {
-        d[(c = columns[i])] = +d[c];
-      }
-      return d;
-    };
-
-    const svg = d3.select('svg');
-    const margin = { top: 20, right: 80, bottom: 30, left: 50 };
-    const width = svg.attr('width') - margin.left - margin.right;
-    const height = svg.attr('height') - margin.top - margin.bottom;
-    const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
-
-    const x = d3.scaleTime().range([0, width]);
-    const y = d3.scaleLinear().range([height, 0]);
-    const z = d3.scaleOrdinal(d3.schemeCategory10);
-
-    const line = d3
-      .line()
-      .curve(d3.curveBasis)
-      .x(d => x(d.date))
-      .y(d => y(d.temperature));
-
-    d3.csv('/public/posts/data.csv', type, (error, data) => {
-      if (error) throw error;
-      console.log(data);
-      const cities = data.columns.slice(1).map(id => ({
-        id,
-        values: data.map(d => ({ date: d.date, temperature: d[id] }))
-      }));
-
-      x.domain(d3.extent(data, d => d.date));
-
-      y.domain([
-        d3.min(cities, c => d3.min(c.values, d => d.temperature)),
-        d3.max(cities, c => d3.max(c.values, d => d.temperature))
-      ]);
-
-      z.domain(cities.map(c => c.id));
-
-      g
-        .append('g')
-        .attr('class', 'axis axis--x')
-        .attr('transform', `translate(0,${height})`)
-        .call(d3.axisBottom(x));
-
-      g
-        .append('g')
-        .attr('class', 'axis axis--y')
-        .call(d3.axisLeft(y))
-        .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 6)
-        .attr('dy', '0.71em')
-        .attr('fill', '#000')
-        .text('Temperature, ºF');
-
-      const city = g
-        .selectAll('.city')
-        .data(cities)
-        .enter()
-        .append('g')
-        .attr('class', 'city');
-
-      city
-        .append('path')
-        .attr('class', 'line')
-        .attr('d', d => line(d.values))
-        .style('stroke', d => z(d.id));
-
-      city
-        .append('text')
-        .datum(d => ({ id: d.id, value: d.values[d.values.length - 1] }))
-        .attr('transform', d => `translate(${x(d.value.date)},${y(d.value.temperature)})`)
-        .attr('x', 3)
-        .attr('dy', '0.35em')
-        .style('font', '10px sans-serif')
-        .text(d => d.id);
-    });
+    const dorta = githubData.concat(spotifyData);
+    this.draw(this.datafy(dorta), this.selectSvg());
   }
 
   componentDidUpdate() {
@@ -117,9 +31,23 @@ class Graph extends Component {
     this.selectAndDraw();
   }
 
-  selectAndDraw = () => {
-    const svg = this.selectSvg();
-    this.draw(this.props, svg);
+  datafy = data => {
+    const returnValue = data.reduce((sum, obj) => {
+      const item = sum.find(
+        thing => d3.timeFormat('%Y-%m-%d')(thing.date) === d3.timeFormat('%Y-%m-%d')(obj.date)
+      );
+
+      if (item) {
+        const sumIndex = sum.findIndex(elem => item === elem);
+        sum[sumIndex] = Object.assign(item, obj);
+      } else {
+        sum.push(obj);
+      }
+
+      return sum;
+    }, []);
+
+    return returnValue;
   };
 
   screenWidth = () =>
@@ -159,64 +87,72 @@ class Graph extends Component {
   //  'date, git, spotify, twitter' as the properties per row.
 
   draw = (data, svg) => {
-    const githubData = this.convertToSimpleData(this.props, 'github');
-    const spotifyData = this.convertToSimpleData(this.props, 'spotify');
     const x = d3.scaleTime().rangeRound([0, svg.width]);
     const y = d3.scaleLinear().rangeRound([svg.height, 0]);
+    const z = d3.scaleOrdinal(d3.schemeCategory10);
+    console.log(data);
 
-    githubData.forEach(d => {
-      d.date = d3.timeParse('%Y-%m-%d')(d.date);
-      d.data = +d.data;
-    });
+    const line = d3
+      .line()
+      .curve(d3.curveBasis)
+      .x(d => x(d.date))
+      .y(d => y(d.temperature));
 
-    spotifyData.forEach(d => {
-      d.date = d3.timeParse('%Y-%m-%d')(d.date);
-      d.data = +d.data;
-    });
+    const cities = ['date', 'Github', 'Spotify'].slice(1).map(id => ({
+      id,
+      values: data.map(d => {
+        const temperature = d[id] ? d[id] : 0;
+        return { date: d.date, temperature };
+      })
+    }));
+    console.log(cities);
+    x.domain(d3.extent(data, d => d.date));
 
-    // temporary fix, need to find the actual max
-    x.domain(d3.extent(githubData, d => d.date));
-    y.domain([0, d3.max(spotifyData, d => Math.max(d.data))]);
+    y.domain([
+      d3.min(cities, c => d3.min(c.values, d => d.temperature)),
+      d3.max(cities, c => d3.max(c.values, d => d.temperature))
+    ]);
+
+    z.domain(cities.map(c => c.id));
 
     svg.g
       .append('g')
+      .attr('class', 'axis axis--x')
       .attr('transform', `translate(0,${svg.height})`)
-      .call(d3.axisBottom(x))
-      .select('.domain');
+      .call(d3.axisBottom(x));
 
     svg.g
       .append('g')
+      .attr('class', 'axis axis--y')
       .call(d3.axisLeft(y))
       .append('text')
-      .attr('fill', '#000')
       .attr('transform', 'rotate(-90)')
       .attr('y', 6)
       .attr('dy', '0.71em')
-      .attr('text-anchor', 'end')
-      .text('contributions');
-    const line = d3
-      .line()
-      .x(d => x(d.date))
-      .y(d => y(d.data));
-    svg.g
-      .append('path')
-      .datum(githubData)
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-linejoin', 'round')
-      .attr('stroke-linecap', 'round')
-      .attr('stroke-width', 3.0)
-      .attr('d', line);
+      .attr('fill', '#000')
+      .text('Temperature, ºF');
 
-    svg.g
+    const city = svg.g
+      .selectAll('.city')
+      .data(cities)
+      .enter()
+      .append('g')
+      .attr('class', 'city');
+
+    city
       .append('path')
-      .datum(spotifyData)
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-linejoin', 'round')
-      .attr('stroke-linecap', 'round')
-      .attr('stroke-width', 3.0)
-      .attr('d', line);
+      .attr('class', 'line')
+      .attr('d', d => line(d.values))
+      .style('stroke', d => z(d.id));
+
+    city
+      .append('text')
+      .datum(d => ({ id: d.id, value: d.values[d.values.length - 1] }))
+      .attr('transform', d => `translate(${x(d.value.date)},${y(d.value.temperature)})`)
+      .attr('x', 3)
+      .attr('dy', '0.35em')
+      .style('font', '10px sans-serif')
+      .text(d => d.id);
   };
 
   render() {
