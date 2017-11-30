936f3e34d3fb7f7f030304bfd7df1cef2ae27e76
karl-thomas <karl.thomas925@gmail.com>
2017-11-29 18:08:20 -0600

    split up top bar into segments to stick a title into the nav

--- js/NavBar.jsx
+++ js/NavBar.jsx
@@ -1,31 +1,68 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import { bool } from 'prop-types';
 import styled from 'styled-components';
 import media, { colors } from './utilities';
 
-const Wrap = styled.div`
-  ${props =>
-    props.startPos
-      ? ` transform: translate(0px, 5vh); 
-          background-color: ${colors.purp};`
-      : ` transform: translate(0px, 77vh);
-          background-color: ${colors.torq};`};
+const Top = styled.div`
+  ${props => (props.startPos ? ` transform: translate(0px, 5vh); ` : ` transform: translate(0px, 77vh);`)};
   z-index: 0;
   position: fixed;
-  color: #ae5d64;
-  width: 100%;
   height: 112px;
-  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  width: 100vw;
   -webkit-transition: all 0.7s ease-out;
   -moz-transition: all 0.7s ease-out;
   -ms-transition: all 0.7s ease-out;
   -o-transition: all 0.7s ease-out;
   transition: all 0.7s ease-out;
+`;
 
+const TopLeft = styled.div`
+  ${props => (props.startPos ? `background-color: ${colors.purp};` : `background-color: ${colors.torq};`)};
+  color: #ae5d64;
+  width: 50%;
+  vertical-align: top;
+  display: inline-block;
+  height: 100%;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
   ${media.phone`display: none;`};
 `;
-// ${media.desktop`background: dodgerblue;`} ${media.tablet`background: mediumseagreen;`}
-const NavBar = (props: { startPos: boolean }) => <Wrap startPos={props.startPos} />;
+
+const TopRight = TopLeft.extend`transform: translate(-50px, 50px);`;
+
+const TopSvg = styled.svg`
+  display: block;
+  left: calc(50vw - 50px);
+  position: fixed;
+  & > .top-triangle {
+    fill: ${colors.torqPurp};
+  }
+  -webkit-transition: all 0.7s ease-out;
+  -moz-transition: all 0.7s ease-out;
+  -ms-transition: all 0.7s ease-out;
+  -o-transition: all 0.7s ease-out;
+  transition: all 0.7s ease-out;
+`;
+
+class NavBar extends Component {
+  static propTypes = {
+    startPos: bool
+  };
+
+  startPos = this.props.startPos;
+
+  render() {
+    return (
+      <Top startPos={this.startPos}>
+        <TopLeft startPos={this.startPos} />
+        <TopSvg>
+          <polygon className="top-triangle" points="50,0 0,0 0,50" />
+        </TopSvg>
+        <TopRight startPos={this.startPos} />
+      </Top>
+    );
+  }
+}
 
 export default NavBar;
