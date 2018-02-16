2033658c145af9996896c43c35ab001049219bb1
karl-thomas <karl.thomas925@gmail.com>
2018-02-16 08:07:43 -0800

    :heavy_plus_sign: add server route for written post show

--- app/routes/api.js
+++ app/routes/api.js
@@ -13,6 +13,15 @@ router.route('/posts').get((req, res) => {
     .catch(() => res.status(400).send('Bad Request'));
 });
 
+router.route('/posts/:slug').get((req, res) => {
+  API(process.env)
+    .Server.Posts.find(req.params.slug)
+    .then(posts => {
+      res.json(posts.data);
+    })
+    .catch(() => res.status(400).send('Bad Request'));
+});
+
 router.use('/api/written', router);
 
 module.exports = router;
